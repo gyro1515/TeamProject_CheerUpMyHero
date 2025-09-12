a22047aa9c7074580a5d34fcecac22c25f682cb5
gyro1515 <gyro1515@naver.com>
2025-09-12 12:50:18 +0900

    [Feat] 1. 스테이지 선택 UI 구현, 2. 스테이지 데이터 구현
    2. 스테이지 데이터 구현 상세 설명
    하나의 배열로 스테이지 데이터 구현,
    메인 스테이지: 이름, 해금여부, 보유 서브 스테이지
    서브 스테이지: 이름, 해금여부

--- Assets/Scripts/UI/UISelecStageSlot.cs
+++ Assets/Scripts/UI/UISelecStageSlot.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+public enum ESelecStageSlotType
+{ Main, Sub}
 public class UISelecStageSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("스테이지 선택 슬롯")]
+    [SerializeField] TextMeshProUGUI stageDisplayText; 
+    [SerializeField] GameObject unlockedImg; 
+    [SerializeField] Button selectButton;
+    [SerializeField] int stageIdx = -1; // 메인 서브 같이 해보기, 얘는 확인용, 인스펙터에서 볼 수 있도록
+    UIStageSelect _uIStageSelect;
+    public Button SelectButton { get { return selectButton; } }
+
+    public void InitSlot(string dispalyText, int idx, bool isUnrocked, UIStageSelect uIStageSelect, ESelecStageSlotType type)
     {
-        
+        stageDisplayText.text = dispalyText;
+        stageIdx = idx;
+        unlockedImg.SetActive(!isUnrocked);
+        _uIStageSelect = uIStageSelect;
+        switch (type)
+        {
+            case ESelecStageSlotType.Main:
+                selectButton.onClick.AddListener(SetSelectedMainSlotIdx);
+                break;
+            case ESelecStageSlotType.Sub:
+                selectButton.onClick.AddListener(SetSelectedSubSlotIdx);
+                break;
+        }
+    }
+    public void SetSlot(string dispalyText, int idx, bool isUnrocked)
+    {
+        stageDisplayText.text = dispalyText;
+        stageIdx = idx;
+        unlockedImg.SetActive(!isUnrocked);
+    }
+    void SetSelectedMainSlotIdx()
+    {
+        _uIStageSelect.SelectedMainSlotIdx = stageIdx;
+    }
+    void SetSelectedSubSlotIdx()
+    {
+        _uIStageSelect.SelectedSubSlotIdx = stageIdx;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
