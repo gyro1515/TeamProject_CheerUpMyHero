2b78798f000a585658ed837a2de6470cdad3645a
Minseok-creat <whalstjr3855@gmail.com>
2025-09-25 10:28:42 +0900

    [Feat] 패시브 유물 착용 슬롯

--- Assets/Scripts/UI/Artifact/UIArtifactEquipSlot.cs
+++ Assets/Scripts/UI/Artifact/UIArtifactEquipSlot.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIArtifactEquipSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("유물 슬롯 타입 : 플 / 원 / 근")]
+    [SerializeField] private EffectTarget _target;
+
+    [Header("인벤토리 UI")]
+    [SerializeField] private UIPassiveArtifactInventory _inventory;
+
+    private Button _button;
+
+    private void Awake()
     {
-        
+        _button = GetComponent<Button>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSlotClicked()
     {
-        
+        if (_inventory != null)
+        {
+            // 인벤토리 여는 메서드 로직 들어가야 함
+        }
     }
 }
