866d4472aabe20b16f604cd60773d83f2eb45952
Kseongmin1107 <ksmsky1107@gmail.com>
2025-10-24 16:22:04 +0900

    [Feat] 가챠 1회 뽑기 디버그로그 띄우기

--- Assets/Scripts/UI/Gacha/GachaUIPanel.cs
+++ Assets/Scripts/UI/Gacha/GachaUIPanel.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-public class GachaUIPanel : BasePopUpUI // BasePopUpUI 상속
+public class GachaUIPanel : BasePopUpUI 
 {
     [Header("UI 참조")]
     [SerializeField] private Button backButton; 
     [SerializeField] private ContractListPanel contractListPanel;
+    [SerializeField] private Button pullOneButton;        // 1회 뽑기 버튼
+    [SerializeField] private Button pullTenButton;        // 10회 뽑기 버튼
 
 
     protected override void Awake()
@@ -13,6 +16,8 @@ public class GachaUIPanel : BasePopUpUI // BasePopUpUI 상속
         base.Awake(); 
 
         backButton?.onClick.AddListener(OnBackButtonClicked);
+        pullOneButton?.onClick.AddListener(OnPullOneClicked);
+        pullTenButton?.onClick.AddListener(OnPullTenClicked);
 
         if (contractListPanel == null)
         {
@@ -24,6 +29,40 @@ public class GachaUIPanel : BasePopUpUI // BasePopUpUI 상속
     {
         base.OnEnable(); 
     }
+    private async void OnPullOneClicked()
+    {
+        pullOneButton.interactable = false;
+        Debug.Log("--- 1회 뽑기 버튼 클릭됨 ---");
+        try
+        {
+            int resultId = await BackendManager.OneNormalGachaAsync();
+            Debug.Log(resultId);
+            PlayerDataManager.Instance.AddResource(ResourceType.Ticket, -1);
+        } 
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+        pullOneButton.interactable = true;
+    }
+
+    private void OnPullTenClicked()
+    {
+        Debug.Log("--- 10회 뽑기 버튼 클릭됨 ---");
+        //pullOneButton.interactable = false;
+    //    try
+    //    {
+    //        int resultId = await BackendManager.OneNormalGachaAsync();
+    //        Debug.Log(resultId);
+    //        PlayerDataManager.Instance.AddResource(ResourceType.Ticket, -1);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Debug.LogException(ex);
+    //    }
+    //    pullOneButton.interactable = true;
+    }
+
 
     private void OnBackButtonClicked()
     {
