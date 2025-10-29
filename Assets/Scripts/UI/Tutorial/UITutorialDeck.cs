19292f144ba3432c091792dbb0cfdda5e6c407fb
Minseok-creat <whalstjr3855@gmail.com>
2025-10-30 03:06:26 +0900

    [Feat] 튜토리얼

---
+++ Assets/Scripts/UI/Tutorial/UITutorialDeck.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITutorialDeck : UITutorialBase
+{
+    protected override void OnSkipButtonClicked()
+    {
+        base.OnSkipButtonClicked();
+    }
+}
