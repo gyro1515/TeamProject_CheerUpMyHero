e984eac2cfebe22a03f7f588c27866d07adaf0f8
Minseok-creat <whalstjr3855@gmail.com>
2025-10-31 10:01:44 +0900

    [Fix] 도전 기능 버그 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -326,6 +326,11 @@ public class GameManager : SingletonMono<GameManager>
             RewardPanelUI?.OpenUI(-penalties.gold, -penalties.wood, -penalties.iron, -penalties.magicStone, false);
         }
 
+        if (PlayerDataManager.Instance != null)
+        {
+            PlayerDataManager.Instance.activeChallenges.Clear();
+        }
+
         await PlayerDataManager.Instance.SaveDataToCloudAsync();
     }
     public async UniTaskVoid ClearStage()
